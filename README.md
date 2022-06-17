abc86584da9c8412dbd23e7473b96fcb5dadb477
april-snowing <1536838236@qq.com>
2022-06-17 22:33:35 +0800

    basic study

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # my-study-javascript
+
+该工程demo全部来自B站后盾人的视频教程，讲的很好，很适合有点基础的同学查漏补缺，提升编程思维
+
+chapter-1 :  起步入门  
+
+https://doc.houdunren.com/js/1%20%E5%9F%BA%E7%A1%80%E7%9F%A5%E8%AF%86.html#%E8%B5%B7%E6%AD%A5%E5%85%A5%E9%97%A8
---
+++ chapter-1/basic.js
@@ -0,0 +1,26 @@
+
+//块级作用域
+// var 会声明一个全局变量，会造成全局变量名污染,不推荐使用，仅供了解
+
+// 理由1
+console.log(window.screenLeft) //输出实际浏览器取屏幕左边的距离
+var screenLeft = 0;
+console.log(window.screenLeft) // var 声明的screnLeft 覆盖
+
+//理由2
+//var会提升到最前面
+var web = "april";
+function hd() {
+    if (false) {
+        var web = "gao"; //if中也会变量提升
+    }
+    console.log(web)  // undefined
+}
+hd();
+//变量提升
+//var a = 1..包含 1.变量的定义  2.变量的复制
+// 变量提升，就是解析器先解析代码，把变量的定义升到最前面
+
+console.log(a);  //undefined  定义
+var a = 1;
+console.log(a); 
