623d6db3452dd6baac3d84e8ba752bc2cf14274e
april-snowing <1536838236@qq.com>
2022-06-17 22:35:49 +0800

    Update basic.js

--- chapter-1/basic.js
+++ chapter-1/basic.js
@@ -17,10 +17,11 @@ function hd() {
     console.log(web)  // undefined
 }
 hd();
+
 //变量提升
-//var a = 1..包含 1.变量的定义  2.变量的复制
+//var a = 1;  包含 1.变量的定义  2.变量的赋值
 // 变量提升，就是解析器先解析代码，把变量的定义升到最前面
 
-console.log(a);  //undefined  定义
+console.log(a);  //undefined 提升到最上面定义
 var a = 1;
 console.log(a); 
